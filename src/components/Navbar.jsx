b2f54d5fcb1123e1d20c88d4e4422e140c0540a9
kirusolo <kirubelendala15@gmail.com>
2023-08-17 11:19:02 +0200

    arangements of my list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,11 +22,23 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p>
+          <p className="text-white text-[18px] font-bold cursor-pointer">
             {" "}
-            Kirubel <span>| Baye </span>
+            Kirubel <span className="sm:block hidden">| Baye </span>
           </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+            >
+              <a href={" #${link.id}"}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
