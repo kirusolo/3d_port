771c194f84c1ef4adc6ed0864a36533f7e7c1f62
kirusolo <kirubelendala15@gmail.com>
2023-08-22 23:31:48 +0200

    put the rotating computer object in the page

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-const Computers = () => {
+const ComputersModel = () => {
+  const { scene } = useGLTF("./desktop_pc/scene.gltf");
+  return <primitive object={scene} />;
+};
+
+const ComputersCanvas = () => {
   return (
-    <div>Computers</div>
-  )
-}
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={null}>
+        <ambientLight intensity={0.5} />
+        <spotLight
+          position={[-20, 50, 10]}
+          angle={Math.PI / 4}
+          penumbra={0.2}
+          intensity={1}
+          castShadow
+        />
+        <ComputersModel />
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
 
-export default Computers
+export default ComputersCanvas;
